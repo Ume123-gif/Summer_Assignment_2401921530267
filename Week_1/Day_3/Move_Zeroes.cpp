76a62753ec5b2cb688a80c094df838aa79bee920
UME KULSOOM <umekulsoom729@gmail.com>
2026-06-03 11:01:07 +0530

    Day 3 Problem 1 done

---
+++ Week_1/Day_3/Move_Zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int temp = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) continue;
+            else {
+                swap (nums[i], nums[temp]);
+                temp++;
+            }
+        }
+    }
+};
