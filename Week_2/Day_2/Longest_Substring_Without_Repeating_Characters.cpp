ceee0e871a1eec1d8f1c4c8361995ff2ac416b53
UME KULSOOM <umekulsoom729@gmail.com>
2026-06-09 18:29:51 +0530

    Week 2 day 2 problem 1

---
+++ Week_2/Day_2/Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,23 @@
+// 1 - Longest substring Without Repeating Characters (Leetcode 3)
+// TC: O(n)
+// SC: O(1)
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxlen = 0;
+        int r = 0;
+        int l = 0;
+        int hash[256];
+        for(int i = 0; i < 256; i++) hash[i] = -1;
+        while (r < s.length()) {
+            if (hash[s[r]] >= l) {
+                l = hash[s[r]] + 1;
+            }
+            maxlen = max(maxlen, r - l + 1);
+            hash[s[r]] = r;
+            r++;
+        }
+        return maxlen;
+    }
+};
