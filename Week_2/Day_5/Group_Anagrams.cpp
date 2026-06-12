dcee50b6ea63bd76aac51c764ae8f2e4add122c4
UME KULSOOM <umekulsoom729@gmail.com>
2026-06-12 22:47:12 +0530

    Week 2 day 5

---
+++ Week_2/Day_5/Group_Anagrams.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map <string, vector<string>> mpp;
+        for (string s : strs) {
+            vector <int> freq (26, 0);
+            for (char c : s) {
+                freq[c-'a']++;
+            }
+            string check = "";
+            for (int i : freq) {
+                check += to_string(i) + "#";
+            }
+            mpp[check].push_back(s);
+        }
+        vector<vector<string>> ans;
+        for (auto it : mpp) {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
